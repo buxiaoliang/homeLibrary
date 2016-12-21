05c00f3c0a7622bae349400d8d683dfed102f8cc
xiaoliang.bu <xiaoliang.bu@hpe.com>
2016-12-21 19:03:38 +0800

    Add location and type management.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,58 @@
 class LocationsController < ApplicationController
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    respond_to do |format|
+      if @location.save
+        flash[:success] = 'New location was successfully created.'
+        format.html { redirect_to @location }
+      else
+        flash[:danger] = 'There was a problem creating the location.'
+        format.html { render :new }
+        format.json { render json: @location.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+
+    respond_to do |format|
+      if @location.update(location_params)
+        flash[:success] = 'New location was successfully updated.'
+        format.html { redirect_to @location }
+      else
+        flash[:danger] = 'There was a problem updating the location.'
+        format.html { render :edit }
+        format.json { render json: @location.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    @location.destroy
+
+    redirect_to locations_path
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  def index
+    @locations = Location.all
+  end
+  # private: need to be at end of class
+  private
+    def location_params
+      params.require(:location).permit(:name, :description)
+    end
 end
--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,2 +1,58 @@
 class TypesController < ApplicationController
+  def new
+    @type = Type.new
+  end
+
+  def create
+    @type = Type.new(type_params)
+    respond_to do |format|
+      if @type.save
+        flash[:success] = 'New type was successfully created.'
+        format.html { redirect_to @type }
+      else
+        flash[:danger] = 'There was a problem creating the type.'
+        format.html { render :new }
+        format.json { render json: @type.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @type = Type.find(params[:id])
+  end
+
+  def update
+    @type = Type.find(params[:id])
+
+    respond_to do |format|
+      if @type.update(type_params)
+        flash[:success] = 'New type was successfully updated.'
+        format.html { redirect_to @type }
+      else
+        flash[:danger] = 'There was a problem updating the type.'
+        format.html { render :edit }
+        format.json { render json: @type.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @type = Type.find(params[:id])
+    @type.destroy
+
+    redirect_to types_path
+  end
+
+  def show
+    @type = Type.find(params[:id])
+  end
+
+  def index
+    @types = Type.all
+  end
+  # private: need to be at end of class
+  private
+  def type_params
+    params.require(:type).permit(:name, :description)
+  end
 end
